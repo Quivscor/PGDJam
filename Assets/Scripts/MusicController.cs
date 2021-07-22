cd1e2b449cf413c1e17fcb770109c672ae986415
B.Krukowski <38346579+OrangeInside@users.noreply.github.com>
2021-07-23 01:54:26 +0200

    Ambient forest + owl hooting

---
+++ Assets/Scripts/MusicController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicController : MonoBehaviour
+{
+
+    [Header("Audio clips")]
+    [SerializeField] private AudioClip[] ambientSounds;
+    [SerializeField] private AudioClip[] fightSounds;
+    [SerializeField] private AudioClip eventSound;
+    [SerializeField] private AudioClip forestAmbient;
+    [SerializeField] private AudioClip howlingAmbient;
+
+    [Header("Audio sources")]
+    [SerializeField] private AudioSource forestSource;
+    [SerializeField] private AudioSource howlingSource;
+
+    private void Start()
+    {
+        forestSource.clip = forestAmbient;
+        forestSource.Play();
+
+        howlingSource.clip = howlingAmbient;
+        howlingSource.Play();
+    }
+
+    private void Update()
+    {
+        //if (!basicSource.isPlaying)
+        //{
+        //    audioSource.clip = clips[currentMusic];
+        //    audioSource.Play();
+        //}
+    }
+    public void PlayAmbientMusic()
+    {
+
+    }
+}
