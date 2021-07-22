ec1b90a5309956de7bdeeaff6591e494fa1eecbe
Quivscor <quivscor@gmail.com>
2021-07-23 00:10:51 +0200

    All leshen rough anims added

--- Assets/PlayerVisuals.cs
+++ Assets/PlayerVisuals.cs
@@ -7,6 +7,8 @@ public class PlayerVisuals : MonoBehaviour
     private Animator animator;
     private SpriteRenderer renderer;
 
+    private bool dashTrigger = false;
+
     private void Awake()
     {
         animator = GetComponentInChildren<Animator>();
@@ -19,5 +21,25 @@ public class PlayerVisuals : MonoBehaviour
             renderer.flipX = false;
         else if (Mathf.Sign(velocity.x) < 0)
             renderer.flipX = true;
+
+        animator.SetFloat("velocityX", velocity.x);
+        animator.SetFloat("velocityY", velocity.y);
+
+        if(state == ActorMovementState.AIRBORNE)
+        {
+            animator.SetTrigger("airborne");
+        }
+
+        if(state == ActorMovementState.DASH)
+        {
+            animator.SetTrigger("dash");
+            dashTrigger = true;
+        }
+
+        //reset flag preventing dash trigger being set every frame
+        if(dashTrigger && state != ActorMovementState.DASH)
+        {
+            dashTrigger = false;
+        }
     }
 }
