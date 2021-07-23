b5fa7b410b5919a7b4d146c0215c02f1fdee35b1
B.Krukowski <38346579+OrangeInside@users.noreply.github.com>
2021-07-23 18:41:06 +0200

    Working on tutorial level

---
+++ Assets/CrowBonus.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrowBonus : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.GetComponentInParent<PlayerMovement>())
+        {
+            StatsController.Instance.UpgradeCrowsMaxNumber();
+            Destroy(this.gameObject);
+        }
+    }
+}
