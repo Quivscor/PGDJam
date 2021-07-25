68ad26627ffbbc64e000a561815615fba21fca56
B.Krukowski <38346579+OrangeInside@users.noreply.github.com>
2021-07-25 23:13:17 +0200

    Ending info

---
+++ Assets/EndingInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndingInfo : MonoBehaviour
+{
+    public Animator animator;
+    public GameObject holder;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.GetComponentInParent<PlayerMovement>())
+        {
+            holder.SetActive(true);
+            PlayerInput.BlockPlayerInput(true);
+            //animator.SetTrigger("Ending");
+        }
+    }
+}
