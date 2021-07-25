9fb8ab48d3cef64a44882f52da1edba0602e4d50
B.Krukowski <38346579+OrangeInside@users.noreply.github.com>
2021-07-25 12:33:45 +0200

    Sanctuarium in progress

--- Assets/ScreenFadeController.cs
+++ Assets/ScreenFadeController.cs
@@ -1,12 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScreenFadeController : MonoBehaviour
 {
     public static ScreenFadeController Instance;
 
-    private void Awake()
+	public Image img;
+	public AnimationCurve curve;
+
+	private void Awake()
     {
         if(Instance == null)
         {
@@ -14,4 +18,37 @@ public class ScreenFadeController : MonoBehaviour
         }
     }
 
+	public void Fade()
+	{
+		StartCoroutine(FadeOut());
+	}
+	IEnumerator FadeIn()
+	{
+		float t = 1f;
+
+		while (t > 0f)
+		{
+			t -= Time.deltaTime;
+			float a = curve.Evaluate(t);
+			img.color = new Color(0f, 0f, 0f, a);
+			yield return 0;
+		}
+	}
+
+	IEnumerator FadeOut()
+	{
+		float t = 0f;
+
+		while (t < 1f)
+		{
+			t += Time.deltaTime;
+			float a = curve.Evaluate(t);
+			img.color = new Color(0f, 0f, 0f, a);
+			yield return 0;
+		}
+
+		//settingsCanvas.SetActive(isPaused);
+		//StartCoroutine(FadeIn());
+
+	}
 }
